ea982c86693d75785d72254ab6707720d1503ba7
mohd ahmad <ahmadseemapuri@gmail.com>
2023-08-14 00:01:04 +0530

    typed is not working

---
+++ home/static/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".typeeffect").typed({
+      strings: ["Software Engineer","Full Stack Developer","Web Designer","Python Enthusiast"],
+      typeSpeed:100,
+      loop:true,
+      backSpeed:50
+    })
+  });
